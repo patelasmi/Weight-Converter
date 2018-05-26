599f9e085350ccb0dc084bb0e5072c438b131fbd
Asmi Patel <30861021+patelasmi@users.noreply.github.com>
2018-05-26 13:18:49 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("output").style.visibility = "hidden";
-document.getElementById("wgt-input").addEventListener("input", function(e) {
-  document.getElementById("output").style.visibility = "visible";
-  let lbs = e.target.value;
-  document.getElementById("grams-op").innerHTML = lbs / 0.0022046;
-  document.getElementById("kilograms-op").innerHTML = lbs / 2.2046;
-  document.getElementById("ounces-op").innerHTML = lbs * 16;
-});
